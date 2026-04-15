a3bbe83a6d12737166f472daa27a3c8d70d50d4f
Owais Hashim <owaishashim41@gmail.com>
2026-04-15 09:52:49 +0500

    Bash scripting practice

---
+++ basic_calc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter 1st num :"
+read num1
+echo "Enter 2nd num:"
+read num2
+sum=$((num1+num2))
+subtract=$((num1-num2))
+multiply=$((num1*num2))
+divide=$((num1/num2))
+echo "Sum = $sum"
+echo "Subtract = $subtract"
+echo "Multiply = $multiply"
+echo "Divide = $divide"
---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello ,Welcome in Bash Scripting"
+#this text is not interpreted by shell
+myname="Owais"
+age=25
+echo "My name is $myname  and my age is  $age"
